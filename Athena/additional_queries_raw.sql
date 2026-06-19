016b75f85225d27e29c5bd4dd1c342dc41780d72
Ian King <148726980+imk24@users.noreply.github.com>
2026-06-19 15:00:51 -0400

    Add additional SQL queries for data analysis

---
+++ Athena/additional_queries_raw.sql
@@ -0,0 +1,30 @@
+--All raw records
+SELECT *
+  FROM raw_example
+  ORDER BY grade ASC;
+
+--Grouping all records by role and grade, counts how many entrys per given role
+SELECT role, grade, COUNT(role) AS Amount 
+  FROM raw_data 
+  GROUP BY role, grade 
+  Order BY grade;
+
+--Groups student records by grade, counts how many entrys per grade
+SELECT grade, COUNT(role) AS student_count
+  FROM raw_data
+  WHERE role LIKE "%Stud%"
+  GROUP BY grade
+  ORDER BY grade;
+
+--Groups teacher records by grade, ounts how many entrys per grade
+SELECT grade, COUNT(role) AS teacher_count
+  FROM raw_data
+  WHERE role LIKE "%Teach%"
+  GROUP BY grade  
+  ORDER BY grade;
+
+--Groups all grades and classes displays the count for each student with same classes
+SELECT grade, class 
+  FROM raw_data
+  GROUP BY grade, class
+  ORDER BY grade;

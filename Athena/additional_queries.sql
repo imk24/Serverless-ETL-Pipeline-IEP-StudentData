30555fff3e3151d52f329ae1f6bcb5d3bd607863
Ian King <148726980+imk24@users.noreply.github.com>
2026-06-19 15:06:42 -0400

    Update and rename additional_queries_raw.sql to additional_queries.sql

--- Athena/additional_queries.sql
+++ Athena/additional_queries.sql
@@ -1,3 +1,4 @@
+---Raw---
 --All raw records
 SELECT *
   FROM raw_example
@@ -28,3 +29,20 @@ SELECT grade, class
   FROM raw_data
   GROUP BY grade, class
   ORDER BY grade;
+
+---Processed---
+--All Processed Records
+SELECT *
+  FROM etl_db.etl_cleaned;
+
+--All Processed Records + Ordered
+SELECT *
+  FROM etl_db.etl_cleaned
+  ORDER BY grade ASC;
+
+--Student Counnt per Grade
+SELECT grade, COUNT(name) AS students
+FROM students
+GROUP BY grade 
+ORDER BY grade ASC; 
+
